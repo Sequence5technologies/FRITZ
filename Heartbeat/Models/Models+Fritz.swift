c85754eb841f85259df406a2d970223d38d8da78
Chris Kelly <ghop02@gmail.com>
2018-08-14 21:24:32 -0400

    Add real api keys

--- Heartbeat/Models/Models+Fritz.swift
+++ Heartbeat/Models/Models+Fritz.swift
@@ -8,22 +8,16 @@
 import Fritz
 
 extension MNIST: SwiftIdentifiedModel {
-
-    static let packagedModelVersion: Int = 1
-
-    static let modelIdentifier: String = "model-id-2"
+    static let modelIdentifier = "8cd3ee61b0d34b8e832977c0455d2d65"
+    static let packagedModelVersion = 1
 }
 
 extension AgeNet: SwiftIdentifiedModel {
-
-    static let packagedModelVersion: Int = 1
-
-    static let modelIdentifier: String = "model-id-3"
+    static let modelIdentifier = "5b376c236b3b40e2826061218c682499"
+    static let packagedModelVersion = 1
 }
 
 extension GenderNet: SwiftIdentifiedModel {
-
-    static let packagedModelVersion: Int = 1
-
-    static let modelIdentifier: String = "model-id-4"
+    static let modelIdentifier = "1aa1620864174fbcbba8efdd17d9dd32"
+    static let packagedModelVersion = 1
 }
